b05a56ee5267a3b76c2618db3cce8e392e134d1c
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2024-04-07 00:43:59 +0200

    replace a elements with span elements and additional script for making span clickable

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,35 @@ jQuery(document).ready(function($) {
     });
 });
 
+function addOnClickToSpans() {
+ // Find all span elements with href property
+ var spansWithHref = document.querySelectorAll('span[href]');
+    
+ // Loop through each span element
+ spansWithHref.forEach(function(span) {
+     // Get the href property value
+     var href = span.getAttribute('href');
+     
+     // Add onclick function
+     span.onclick = function() {
+         // Open a new blank page with the href URL
+         window.open(href, '_blank');
+     };
+     
+     // Add mouseover event to change cursor to hand
+     span.addEventListener('mouseover', function() {
+         this.style.cursor = 'pointer';
+     });
+     
+     // Add mouseout event to revert cursor back to default
+     span.addEventListener('mouseout', function() {
+         this.style.cursor = 'auto';
+     });
+ });
+}
+
+
+// Call the function when the document is ready
+document.addEventListener('DOMContentLoaded', function() {
+    addOnClickToSpans();
+});
